3d54549723944ad78a82627d95f0461924c9119f
Ayush Rathore <aayushh79@gmail.com>
2022-01-19 18:05:25 +0530

    Update config script

--- scripts/update-config.sh
+++ scripts/update-config.sh
@@ -6,5 +6,6 @@ cp -rv ${HOME}/.config/zsh/functions ../zsh/
 cp -rv ${HOME}/.config/zsh/settings.zsh ../zsh/
 cp -rv ${HOME}/.config/micro/*.json ../micro/
 cp -rv ${HOME}/.config/lsd ../
+cp -rv ${HOME}/.config/mpv ../
 cp -rv ${HOME}/.config/starship.toml ../
 cp -rv ${HOME}/.config/spicetify/config-xpui.ini ../spicetify
