ebf60bd5f71ab82def3b8a6f5119db406acb8d01
Ayush Rathore <aayushh79@gmail.com>
2022-05-13 15:53:50 +0530

    Don't copy unncecessary stuff

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,20 +1,16 @@
-# zsh config files
+# zsh 
 cp -frv $HOME/.config/zsh/functions ../.config/zsh/
 cp -frv $HOME/.config/zsh/settings.zsh ../.config/zsh/
 cp -frv $HOME/.config/zsh/.zshrc ../.config/zsh/
 cp -frv $HOME/.config/zsh/.zshenv ../.config/zsh/
 cp -frv $HOME/.config/zsh/.zprofile ../.config/zsh/
 
-# copy config files
+# config files
 cp -frv \
 	$HOME/.config/alacritty \
-	$HOME/.config/btop \
 	$HOME/.config/dunst \
 	$HOME/.config/feh \
-	$HOME/.config/fontconfig \
-	$HOME/.config/gtk-3.0 \
 	$HOME/.config/lsd \
-	$HOME/.config/mpv \
 	$HOME/.config/neofetch \
 	$HOME/.config/sway \
 	$HOME/.config/swaylock \
@@ -22,3 +18,4 @@ cp -frv \
 	$HOME/.config/wofi \
 	$HOME/.config/starship.toml \
 	../.config/
+
